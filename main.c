a7cef2e72aefbc74e697c1a3ed7824b1420e1c10
vinitgaikwad-024 <vinitgaikwad024@gmail.com>
2025-11-10 22:33:18 +0530

    vinit work

--- main.c
+++ main.c
@@ -5,8 +5,68 @@ void display(char *x,int total, int n){
     printf("\n[%s] Total Head Movement: %d | Average Seek Time: %.2f\n",
            x, total, (float)total / n);
 }
-int fcfs(); // VINIT
-int scan();//vinit
+int fcfs(){
+    int n, i, head, total_seek = 0;
+
+    printf("Enter number of disk requests: ");
+    scanf("%d", &n);
+
+    int request[n];
+    printf("Enter the disk request sequence:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &request[i]);
+    }
+
+    printf("Enter initial head position: ");
+    scanf("%d", &head);
+
+    printf("\nFCFS Disk Scheduling:\n");
+    printf("Sequence of movement: %d", head);
+
+    for (i = 0; i < n; i++) {
+        printf(" -> %d", request[i]);
+        total_seek += abs(request[i] - head);
+        head = request[i];
+    }
+
+    printf("\nTotal Seek Time = %d", total_seek);
+    printf("\nAverage Seek Time = %.2f\n", (float)total_seek / n);
+
+    return 0;
+}
+void scan(int arr[], int n, int head, int disk_size, int direction) {
+    int seek_count = 0, distance, cur_track;
+    int temp[n + 1], i, j;
+
+    for (i = 0; i < n; i++) temp[i] = arr[i];
+    temp[n] = head;
+    n++;
+
+    // Sort the requests
+    for (i = 0; i < n - 1; i++)
+        for (j = i + 1; j < n; j++)
+            if (temp[i] > temp[j]) {
+                int t = temp[i];
+                temp[i] = temp[j];
+                temp[j] = t;
+            }
+
+    int pos;
+    for (i = 0; i < n; i++)
+        if (temp[i] == head) { pos = i; break; }
+
+    printf("Seek sequence: ");
+    if (direction == 1) { // Right
+        for (i = pos; i < n; i++) printf("%d ", temp[i]);
+        printf("%d ", disk_size - 1);
+        for (i = pos - 1; i >= 0; i--) printf("%d ", temp[i]);
+    } else { // Left
+        for (i = pos; i >= 0; i--) printf("%d ", temp[i]);
+        printf("0 ");
+        for (i = pos + 1; i < n; i++) printf("%d ", temp[i]);
+    }
+    printf("\n");
+}
 int clook(int req[],int n,int head){
 
      int total = 0, arr[MAX + 1];
@@ -46,7 +106,34 @@ int look(int req[],int n,int head){
 
 
 }
-int cscan();//
+void cscan(int arr[], int n, int head, int disk_size) {
+    int seek_count = 0, i, j;
+    int temp[n + 2];
+
+    for (i = 0; i < n; i++) temp[i] = arr[i];
+    temp[n] = head;
+    temp[n + 1] = disk_size - 1; // end point
+    n += 2;
+
+    // Sort
+    for (i = 0; i < n - 1; i++)
+        for (j = i + 1; j < n; j++)
+            if (temp[i] > temp[j]) {
+                int t = temp[i];
+                temp[i] = temp[j];
+                temp[j] = t;
+            }
+
+    int pos;
+    for (i = 0; i < n; i++)
+        if (temp[i] == head) { pos = i; break; }
+
+    printf("Seek sequence: ");
+    for (i = pos; i < n; i++) printf("%d ", temp[i]);
+    printf("0 ");
+    for (i = 0; i < pos; i++) printf("%d ", temp[i]);
+    printf("\n");
+}
 int sstf(int req[], int n, int head) {
     int done[MAX] = {0}, total = 0;
     for (int count = 0; count < n; count++) {
